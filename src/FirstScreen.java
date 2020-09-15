1112b1b2096f8714f7ac05b993aae727b1013d56
thayes8 <57459883+thayes8@users.noreply.github.com>
2020-09-14 23:23:13 -0500

    Big Progress to the card saving screen. Saves and prints the GiftCard object saved.

--- src/FirstScreen.java
+++ src/FirstScreen.java
@@ -18,9 +18,12 @@ public class FirstScreen extends Application implements EventHandler<ActionEvent
 
     Button button;
     Scene sceneOne;
-
     Button saveButton;
     TextField cardNumber;
+    TextField cvv;
+    TextField expirationMonth;
+    TextField expirationYear;
+    TextField cardName;
     Button backButton;
     Scene sceneTwo;
 
@@ -45,18 +48,39 @@ public class FirstScreen extends Application implements EventHandler<ActionEvent
         stage.show();
 
         saveButton = new Button("Add Card");
-        cardNumber = new TextField("Enter a Gift Card Number");
+        cardName = new TextField("Store Name");
+        cardNumber = new TextField("Enter a Gift Card #");
+        cvv = new TextField("Enter 3 digit CVV #");
+        expirationMonth = new TextField("Expiration Month ex: '09");
+        expirationYear = new TextField("Expiration Year ex: '23'");
         backButton = new Button("Back");
         StackPane layout2 = new StackPane();
 
+        layout2.getChildren().add(cardName);
+        layout2.getChildren().add(cvv);
+        layout2.getChildren().add(expirationMonth);
+        layout2.getChildren().add(expirationYear);
+        StackPane.setAlignment(cardName, Pos.TOP_CENTER);
+        StackPane.setAlignment(cvv, Pos.TOP_CENTER);
+        StackPane.setAlignment(expirationMonth, Pos.TOP_CENTER);
+        StackPane.setAlignment(expirationYear, Pos.TOP_CENTER);
+        StackPane.setMargin(cardName, new Insets(120, 0, 0, 0));
+        StackPane.setMargin(cvv, new Insets(150, 0, 0, 0));
+        StackPane.setMargin(expirationMonth, new Insets(180, 0, 0, 0));
+        StackPane.setMargin(expirationYear, new Insets(210, 0, 0, 0));
+
+
+
         saveButton.setOnAction(actionEvent -> {
-            cardList.add(new GiftCard(cardNumber.getText()));
-            Label label = new Label("Card number: " + cardNumber.getText());
+            GiftCard card = new GiftCard(cardName.getText(), cardNumber.getText(), cvv.getText(), expirationMonth.getText() + "/" + expirationYear);
+            cardList.add(card);
+            Label label = new Label(cardName.getText() + " Card Saved!");
             cardLabelList.add(label);
             StackPane.setAlignment(label, Pos.TOP_CENTER);
             StackPane.setMargin(label, new Insets(counter));
             counter+=20;
             layout2.getChildren().add(label);
+            System.out.println(card.toString());
         });
         backButton.setOnAction(actionEvent -> {
             for(Label label: cardLabelList){
@@ -64,12 +88,18 @@ public class FirstScreen extends Application implements EventHandler<ActionEvent
             }
             counter = 0;
             stage.setScene(sceneOne);
+            cardName.setText("What store is this card for?");
+            cardNumber.setText("Enter a Gift Card #");
+            cvv.setText("Enter 3 digit CVV #");
+            expirationMonth.setText("Expiration Month ex: '09");
+            expirationYear.setText("Expiration Year ex: '23'");
         });
 
         layout2.getChildren().add(backButton);
         StackPane.setAlignment(backButton, Pos.TOP_LEFT);
         layout2.getChildren().add(cardNumber);
-        StackPane.setAlignment(cardNumber, Pos.CENTER);
+        StackPane.setAlignment(cardNumber, Pos.TOP_CENTER);
+        StackPane.setMargin(cardNumber, new Insets(240, 0, 0, 0));
         layout2.getChildren().add(saveButton);
         StackPane.setAlignment(saveButton, Pos.BOTTOM_CENTER);
         sceneTwo = new Scene(layout2, 300, 500);
--- src/GiftCard.java
+++ src/GiftCard.java
@@ -1,9 +1,27 @@
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
 public class GiftCard {
     ImageView barCodeImage;
     String cardNumber;
-    public GiftCard(String cardNumber){
+    String cvv;
+    String expiration;
+    String cardName;
+    public GiftCard(String cardName, String cardNumber, String cvv, String expiration){
+        this.cardName = cardName;
         this.cardNumber = cardNumber;
+        this.cvv = cvv;
+        this.expiration = expiration;
+    }
+
+    @Override
+    public String toString() {
+        return "GiftCard{" +
+                "barCodeImage=" + barCodeImage +
+                ", cardName='" + cardName + '\'' +
+                ", cardNumber='" + cardNumber + '\'' +
+                ", cvv='" + cvv + '\'' +
+                ", expiration='" + expiration + '\'' +
+                '}';
     }
 }
